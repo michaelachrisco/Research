505f4bbe2e2fcc3fce22c131921deb9b6997264d
Michael Chrisco <michaelachrisco@gmail.com>
2011-04-07 11:23:51 -0700

    The not so impressive 99 bottles of beer algorithm with a recursive taste. Was done for rosettacode a while back.

---
+++ other/ninenineBottles.cpp
@@ -0,0 +1,27 @@
+//As seen on Rosettacode implamentation of 99 bottles of beer with recursion. 
+
+#include <iostream>
+using namespace std;
+void rec(int bottles)
+{
+if ( bottles!=0)    
+ {    
+     cout << bottles << " bottles of beer on the wall" << endl; 
+        cout << bottles << " bottles of beer" << endl;
+        cout << "Take one down, pass it around" << endl; 
+        cout << --bottles << " bottles of beer on the wall\n" << endl;
+    
+    rec(bottles);
+ }  
+
+
+}
+
+
+ int main() 
+ {   
+rec(99);
+system("pause");
+return 0;
+}
+
